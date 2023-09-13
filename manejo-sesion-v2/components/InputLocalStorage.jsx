4b10ecc26c3429acf78903617a8b2131e817f02d
Aluon <paulacanoroman@gmail.com>
2023-09-13 10:50:25 +0200

    Recuperar sesion función y lógica en input para limpiarse

--- manejo-sesion-v2/components/InputLocalStorage.jsx
+++ manejo-sesion-v2/components/InputLocalStorage.jsx
@@ -9,13 +9,19 @@ const InputLocalStorage = () => {
         setUserData("")
     }
 
+    const handleRecoverData = ()=>{
+        const datosLocal = localStorage.getItem("datos usuario")
+        setUserData(JSON.parse(datosLocal))
+    }
+
 
   return (
     <>
-    <input type='text' onChange={(e)=>{setUserData({...userData, username: e.target.value})}}></input>
-    <input type='password' onChange={(e)=>{setUserData({...userData, password: e.target.value})}}></input>
+    <input type='text' onChange={(e)=>{setUserData({...userData, username: e.target.value})}} value={userData.username ?? ""}></input>
+    <input type='password' onChange={(e)=>{setUserData({...userData, password: e.target.value})}} value={userData.password ?? ""}></input>
 
     <button onClick={()=>{handleSaveData()}}>Iniciar sesión</button>
+    <button onClick={()=>{handleRecoverData()}}>Recuperar sesión</button>
     </>
   )
 }
